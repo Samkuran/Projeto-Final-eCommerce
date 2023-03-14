c9b54d55015eb50d46b2354655bfe8a1c1bbe7fb
Samantha Pimentel <samthvet@hotmail.com>
2023-03-13 23:48:35 -0300

    atualizacao navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,52 @@
 import vetor from "./Vector.png";
-import {Button, TextField, Alert, Badge} from "@mui/material";
-import {Favorite, Group, ShoppingCart} from "@mui/icons-material";
-
+import {TextField, InputAdornment, Button, Badge} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import "./styles.css";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+
 
 export default function Navbar (){
     return(
+        <>
         <div className="container">
             <section id="conjlogo">
-                <img id="vetor" src={vetor}/>
-                <a href="" id="logo">Digital Store</a>
-             </section>
-
-             <TextField  className="busca" label="Pesquisar produto..." 
+                <img id="vetor" alt="logo da Digital College" src={vetor}/>
+                <a href="newtab" id="logo">Digital Store</a>
+            </section>
+            
+            <TextField color="secondary"
             InputProps={{
-                startAdornment: (
-                <InputAdornment position="end">
-                    <AccountCircle/>
-                </InputAdornment>
-                ),
+                endAdornment: <InputAdornment position="end"><SearchIcon/></InputAdornment>,}}
+            sx={{
+                width: 560,
+                marginLeft: 3,
                 }}
-                variant="standard"
-            /> 
+            label="Pesquisar produto..."/>    
 
-            <Button color='secondary' variant="contained">Entrar</Button>
+            <a href="newtab" className="cadastro">Cadastre-se</a>
+
+            <Button sx={{
+                width: 114, 
+                height: 40,
+                color: "white",
+                bgcolor: "#C92071",
+                "&:hover": {
+                    color: "white",
+                    bgcolor: "#7c1447",
+                    transition: 300,
+                },
+            }}>Entrar</Button>
+        <div className="carrinho">
+            <Badge badgeContent={4}>
+                <ShoppingCartOutlinedIcon sx={{color: "#C92071",}}/>
+            </Badge>
+        </div>
+           
            
         </div>
+   
+        </>
     );
 }
+
